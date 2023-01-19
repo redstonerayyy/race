c297ec21d7c6462f3e6966ac102556e60ee1b720
Redstonerayy <antonpaulr@protonmail.com>
2023-01-19 21:19:37 +0100

    structure in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::fs;
-
+// lex files and get tokens
 mod lexer;
 use crate::lexer::lexer::*;
+
+// read files
+use std::fs;
 
 fn main() {
     // read file
     let file_path = String::from("test/main.race");
     let contents = fs::read_to_string(file_path).expect("Could not read file!");
-    println!("With text:\n{contents}");
-    //
+
+    // lex file
     let lex = Lexer::new(contents);
     lex.start();
-    lex.getsource();
 }
